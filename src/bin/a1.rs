c8c98c0bae6f045ad52181292c8db3fe51e99fc3
muendokennedy <kennedymunyao999@gmail.com>
2025-06-05 13:19:04 +0300

    Made a simple function that display your full name using functions

---
+++ src/bin/a1.rs
@@ -0,0 +1,15 @@
+fn first_name(){
+  println!("Kennedy");
+}
+
+fn last_name(){
+  println!("Muendo");
+}
+
+
+fn main(){
+
+  first_name();
+  last_name();
+
+}
--- src/main.rs
+++
@@ -1,8 +0,0 @@
-fn main(){
-
-    let x :i8 = 16;
-
-    println!("The number that I wrote is {:?}", x);
-
-
-}
